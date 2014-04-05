4af3be07694c8f45b929b39b8900f0c1bb3a2c38
Yoshihiro Furukawa <yoshihiro.furukawa@nifty.com>
2014-04-05 21:50:08 +0900

    update comment

--- ForGDB/Sorter.cpp
+++ ForGDB/Sorter.cpp
@@ -41,28 +41,28 @@ void Sorter::scootOver(int position) {
 
 void Sorter::insert(int data) {
     int j;
-    if (currentPosition == 0) { // 単純に、yが空の場合
+    if (currentPosition == 0) { // 単純に、currentPositionが空の場合
     	sortedData[0] = data;
         return;
     }
-    // new_yを越える最初の要素に対する位置yの直前に
+    // dataを越える最初の要素に対する位置の直前に
     // 挿入する必要がある
     for (j = 0; j < currentPosition; j++) {
         if (data < sortedData[j]) {
-            // new_yを挿入する前に、
-            // y[j], y[j+1],... と右に移動する
+            // dataを挿入する前に、
+            // 右に移動する
             scootOver(j);
             sortedData[j] = data;
             return;
         }
-        // もうひとつの場合： new_y > 既存のyの全要素
+        // もうひとつの場合： data > 既存のsortedDataの全要素
         sortedData[currentPosition] = data;
     }
 }
 
 void Sorter::processData() {
     for (currentPosition = 0; currentPosition < numberOfInputData; ++currentPosition)
-        // new y をy[0],...,y[num_y-1]の間の
+        // data をsortedData[0],...,sortedData[numberOfInputData-1]の間の
         // 適切な場所に挿入する
         insert(inputDatas[currentPosition]);
 }
